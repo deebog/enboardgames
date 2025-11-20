b4e16a1996d12f9f940a70ef6f2209152602031c
deebog <denis.a.bogachev@gmail.com>
2025-11-20 09:51:13 +0300

    Delete bot/jobs.py

--- bot/jobs.py
+++
@@ -1,6 +0,0 @@
-from telegram.ext import ContextTypes
-import datetime
-
-async def reminder(context: ContextTypes.DEFAULT_TYPE):
-    job = context.job
-    await context.bot.send_message(job.chat_id, text=f"Напоминание о встрече: {job.data}")
