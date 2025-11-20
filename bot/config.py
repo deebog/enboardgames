52fa4f040fa5f2a700e8c99750172cf9f9eb3e3f
deebog <denis.a.bogachev@gmail.com>
2025-11-20 09:01:11 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,5 +1,5 @@
 import os
 
-TOKEN = os.getenv("6921821959:AAEKOByzSM93fXPCbJYAahwE63ng86pUGBs")
+TOKEN = os.getenv("TG_BOT_TOKEN")
 ADMIN_IDS = [200615203]   # Replace with your admin Telegram user ID(s)
 DB_PATH = "sessions.db"
