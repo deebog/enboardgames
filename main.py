dcd37381e201f31db8fd8a6bf949f6e747b1f2e2
deebog <denis.a.bogachev@gmail.com>
2025-11-20 11:59:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-from telegram.ext import ApplicationBuilder
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler
 import config
 from handlers import start, list_sessions, list_games, button_handler
 
 def main():
     app = ApplicationBuilder().token(config.TOKEN).build()
 
-    app.add_handler(start)
-    app.add_handler(list_sessions)
-    app.add_handler(list_games)
-    app.add_handler(button_handler)
+    # Команды
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("sessions", list_sessions))
+    app.add_handler(CommandHandler("games", list_games))
+
+    # Обработчик всех кнопок
+    app.add_handler(CallbackQueryHandler(button_handler))
 
     app.run_polling()
 
